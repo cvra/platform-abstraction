67b9b616cc2e0ea431daf0de5b1cab28b3f3d4ef
Antoine Albertelli <antoinea101@gmail.com>
2014-06-22 17:34:59 +0200

    Added static allocation of stacks.

--- tests/threading_test.cpp
+++ tests/threading_test.cpp
@@ -3,3 +3,26 @@
 extern "C" {
 #include "../threading.h"
 }
+
+TEST_GROUP(ThreadingAPITestGroup)
+{
+};
+
+TEST(ThreadingAPITestGroup, CanAllocateStackStatically)
+{
+    THREAD_STACK(mystack, 2048);
+    CHECK_EQUAL(sizeof(thread_stack_t), sizeof(mystack));
+}
+
+TEST(ThreadingAPITestGroup, StaticAllocationFillsSizeField)
+{
+    THREAD_STACK(mystack, 2048);
+    CHECK_EQUAL(2048, mystack.size);
+}
+
+TEST(ThreadingAPITestGroup, CanAccessMiddleOfStack)
+{
+    THREAD_STACK(mystack, 2048);
+    mystack.stack[1024] = 42; /* should not crash */
+    CHECK_EQUAL(mystack.stack[1024], 42);
+}
--- threading.h
+++ threading.h
@@ -1,5 +1,18 @@
 #ifndef THREADING_H_
 #define THREADING_H_
 
+#include <stdlib.h>
+#include <stdint.h>
+
+/** Structure representing a thread stack. */
+typedef struct {
+    size_t size;
+    uint8_t *stack;
+} thread_stack_t;
+
+/** Statically allocates some space and fills the given stack structure. */
+#define THREAD_STACK(var, s) uint8_t __stack_ ## var [s];  \
+    thread_stack_t var = {.size = s, .stack= __stack_ ## var };
+
 
 #endif
