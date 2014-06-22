103b7bcf1ba87232090319e13d161098389b1c9e
Antoine Albertelli <antoinea101@gmail.com>
2014-06-22 20:49:05 +0000

    Removed designated initializer.
    This is only supported by C, not C++, which caused problems in Travis
    CI. The fun part is that g++>4.7 supports them and so does clang++...

--- threading.h
+++ threading.h
@@ -12,7 +12,7 @@ typedef struct {
 
 /** Statically allocates some space and fills the given stack structure. */
 #define THREAD_STACK(var, s) uint8_t __stack_ ## var [s];  \
-    thread_stack_t var = {.size = s, .stack= __stack_ ## var };
+    thread_stack_t var = {s, __stack_ ## var };
 
 /** Dynamically allocates the memory for a stack on the heap. */
 thread_stack_t *thread_stack_create(size_t size);
