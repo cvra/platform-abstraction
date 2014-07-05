551bcfbdbd497a32803ffb5644fd75a4b14dfd8a
Antoine Albertelli <antoinea101@gmail.com>
2014-07-05 20:10:27 +0200

    xmalloc: refactored tests.

--- tests/malloc_test.cpp
+++ tests/malloc_test.cpp
@@ -13,51 +13,56 @@ static void panic_counter(const char *file, int line, const char *msg)
 }
 
 TEST_GROUP(MallocTestGroup) {
+    int *array;
+
+    /* Find max value of size_t */
+    static const size_t max_size = (size_t) -1;
+
     void setup()
     {
         UT_PTR_SET(panic, panic_counter);
         panic_count = 0;
+        array = NULL;
+    }
+
+    void teardown()
+    {
+        xfree(array);
     }
 };
 
 TEST(MallocTestGroup, CanAllocateMemory)
 {
-    int *array = (int *)xmalloc(10*sizeof(int));
+    array = (int *)xmalloc(10*sizeof(int));
     array[9] = 42;
     CHECK_EQUAL(42, array[9]);
-    xfree(array);
 }
 
 TEST(MallocTestGroup, CreatingHugeAmountOfMemoryFails)
 {
-    /* Find max value of size_t */
-    size_t max_size = (size_t) -1;
-
     xmalloc(max_size);
     CHECK_EQUAL(1, panic_count);
 }
 
 TEST(MallocTestGroup, ZeroSizeDoesntCrash)
 {
-    void *ptr = xmalloc(0);
+    xmalloc(0);
     CHECK_EQUAL(0, panic_count);
 }
 
 TEST(MallocTestGroup, XReallocWorksToo)
 {
-    int *ptr = (int *)xmalloc(10*sizeof(int));
-    ptr[9] = 42;
-    ptr = (int *)xrealloc(ptr, 100);
-    ptr[99] = 43;
+    int *array = (int *)xmalloc(10*sizeof(int));
+    array[9] = 42;
+    array = (int *)xrealloc(array, 100);
+    array[99] = 43;
 
-    CHECK_EQUAL(42, ptr[9]);
-    CHECK_EQUAL(43, ptr[99]);
-    xfree(ptr);
+    CHECK_EQUAL(42, array[9]);
+    CHECK_EQUAL(43, array[99]);
 }
 
 TEST(MallocTestGroup, XReallocCanFailOnHugeSize)
 {
-    size_t max_size = (size_t) -1;
-    int *ptr = (int *)xrealloc(NULL, max_size);
+    xrealloc(NULL, max_size);
     CHECK_EQUAL(1, panic_count);
 }
