c3f3c63f14cb8d4790668ea4b172ef15e0ff3d34
Michael Spieler <the.chaotic.coder@gmail.com>
2014-08-26 16:55:54 +0200

    Consistent comment style.

--- semaphores.h
+++ semaphores.h
@@ -10,19 +10,19 @@
 #include "ucos-iii/semaphore.h"
 #endif
 
-/** Initializes a semaphore with given count value. */
+/** Initializes the semaphore with given count value. */
 void os_semaphore_init(semaphore_t *sem, uint32_t count);
 
-/** Takes a semaphore if available, blocks if not available. */
+/** Takes the semaphore if available, blocks if not available. */
 void os_semaphore_take(semaphore_t *sem);
 
-/** Takes a semaphore non-blocking */
+/** Takes the semaphore non-blocking */
 bool os_semaphore_try(semaphore_t *sem);
 
 /** Acquire the semaphore blocking with timeout [us] */
 bool os_semaphore_try_timeout(semaphore_t *sem, uint32_t timeout);
 
-/** Releases (posts) a semaphore. */
+/** Releases (posts) the semaphore. */
 void os_semaphore_release(semaphore_t *sem);
 
 #endif
