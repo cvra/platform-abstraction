58ad47ec61a6d7ab26a7e2eac38e500b8c0bc2b2
Michael Spieler <the.chaotic.coder@gmail.com>
2014-08-26 20:08:10 +0200

    Added threading header

---
+++ threading.h
@@ -0,0 +1,25 @@
+#ifndef THREADING_H_
+#define THREADING_H_
+
+#include <stdint.h>
+#include <stddef.h>
+
+#ifdef __unix__
+#include "mock/threading.h"
+#else
+#include "ucos-iii/threading.h"
+#endif
+
+/** Initializes OS. */
+void os_init(void);
+
+/** Starts scheduling the created threads. Never returns */
+void os_run(void);
+
+/** Creates a new thread. */
+void os_thread_create(os_thread_t *thread, void (*fn)(void *), void *stack, size_t stack_size, const char *name, unsigned int prio, void *arg);
+
+/** Sleep for n milliseconds */
+void os_thread_sleep(uint32_t millisec);
+
+#endif /* THREADING_H_ */
