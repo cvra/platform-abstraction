5e424a8c8b795304ed432f89b28624ad73039e00
Florian Reinhard <florian.reinhard@epfl.ch>
2014-10-01 18:43:48 +0200

    criticism on critical sections

--- criticalsection.h
+++ criticalsection.h
@@ -16,19 +16,11 @@ extern "C" {
 
 
 /** Takes care of allocs for critical section */
-#define CRITICAL_SECTION_ALLOC() int16_t __critctrl; int16_t __critical_alloc; CPU_SR_ALLOC();
+#define CRITICAL_SECTION_ALLOC() CPU_SR_ALLOC();
 
-#define CRITICAL_SECTION() for(__critctrl = -1; __critctrl < 2 && sizeof(__critical_alloc)>0; __critctrl++) \
-                                                    if(__critctrl < 0) \
-        {    CPU_CRITICAL_ENTER(); } \
-        else if(__critctrl > 0) \
-        {   CPU_CRITICAL_EXIT(); __critctrl = 0; break; }\
-        else for(__critical_alloc = 0; __critical_alloc<1; __critical_alloc++)
+#define CRITICAL_SECTION_ENTER() CPU_CRITICAL_ENTER();
 
-
-#define CRITICAL_SECTION_ENTER() {if(sizeof(__critical_alloc)>0){CPU_CRITICAL_ENTER();(void) __critctrl;}}
-
-#define CRITICAL_SECTION_EXIT() {if(sizeof(__critical_alloc)>0){CPU_CRITICAL_EXIT();}}
+#define CRITICAL_SECTION_EXIT() CPU_CRITICAL_EXIT();
 
 #ifdef __cplusplus
 }
